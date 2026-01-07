516a60f6cd43f0704b03c66c4cd48e24b4fb06e2
koma712 <koma712@users.noreply.github.com>
2026-01-07 23:26:54 +0900

    fix: ensure image cleanup on every yazi session close

--- lua/yazi/integrations/snacks_image_preview.lua
+++ lua/yazi/integrations/snacks_image_preview.lua
@@ -51,19 +51,20 @@ function M.handle_hover(url, yazi_win_id)
 
     hide_loading_text()
 
+    -- [Bug Fix] 매 세션마다 yazi 버퍼에 대한 종료 감지기를 등록해야 함
+    local yazi_buf = vim.api.nvim_win_get_buf(yazi_win_id)
+    if not vim.b[yazi_buf].yazi_snacks_attached then
+      vim.b[yazi_buf].yazi_snacks_attached = true
+      vim.api.nvim_create_autocmd({ "BufWipeout", "WinClosed", "VimLeavePre" }, {
+        buffer = yazi_buf,
+        callback = function() M.close_preview() end,
+        once = true,
+      })
+    end
+
     if not M.preview_buf or not vim.api.nvim_buf_is_valid(M.preview_buf) then
       M.preview_buf = vim.api.nvim_create_buf(false, true)
       pcall(vim.api.nvim_set_option_value, "bufhidden", "hide", { buf = M.preview_buf })
-      
-      local yazi_buf = vim.api.nvim_win_get_buf(yazi_win_id)
-      if not vim.b[yazi_buf].yazi_snacks_attached then
-        vim.b[yazi_buf].yazi_snacks_attached = true
-        vim.api.nvim_create_autocmd({ "BufWipeout", "WinClosed", "VimLeavePre" }, {
-          buffer = yazi_buf,
-          callback = function() M.close_preview() end,
-          once = true,
-        })
-      end
     end
     
     local buf = M.preview_buf
@@ -127,8 +128,6 @@ function M.handle_hover(url, yazi_win_id)
       if ok and rid == M.request_id then
         M.current_placement = placement
         
-        -- [Hack] 고해상도 이미지의 DPI를 표준(96)으로 강제 고정하여 크기 축소 방지
-        -- dpi 필드가 nil이면 크래시가 나므로 {width=96, height=96}으로 설정
         if placement.img then
           local original_on_ready = placement.img.on_ready
           placement.img.on_ready = function(self)
@@ -136,9 +135,14 @@ function M.handle_hover(url, yazi_win_id)
                 self.info.dpi = { width = 96, height = 96 }
             end
             if original_on_ready then original_on_ready(self) end
+            
+            vim.schedule(function()
+              if placement then placement:update() end
+            end)
           end
           if placement.img.info then 
               placement.img.info.dpi = { width = 96, height = 96 }
+              placement:update()
           end
         end
         
